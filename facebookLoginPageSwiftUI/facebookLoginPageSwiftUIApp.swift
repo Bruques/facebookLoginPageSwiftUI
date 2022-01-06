8f692685c652594c1a4f74f0ab8627b73c51e488
Bruno Nascimento Marques <mv.brunonmarques@gmail.com>
2022-01-06 08:05:10 -0300

    Initial Commit

---
+++ facebookLoginPageSwiftUI/facebookLoginPageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebookLoginPageSwiftUIApp.swift
+//  facebookLoginPageSwiftUI
+//
+//  Created by Bruno Nascimento Marques on 05/01/22.
+//
+
+import SwiftUI
+
+@main
+struct facebookLoginPageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
